b8ef8ae4560f49698db3039f1d6828a74be85817
michelleydaley <mdaley.hhs@gmail.com>
2016-11-03 21:36:27 -0400

    created an item and ability to take it

--- utilities.js
+++ utilities.js
@@ -11,9 +11,24 @@ function location(id, name, desc, descAfter, item) {
 		}
 }
 
-var river = new location(0, "The River", "5. You're drowning in the river. But there is a sturgeon! Get out quick!","5. You're drowning in the river. No fish to be found just dirty needles :( Get out quick!", null);
-var hancock = new location(1, "Hancock", "5. You're drowning in the river. But there is a sturgeon! Get out quick!","5. You're drowning in the river. No fish to be found just dirty needles :( Get out quick!", null);
-var gym = new location(2, "Gym", "5. You're drowning in the river. But there is a sturgeon! Get out quick!","5. You're drowning in the river. No fish to be found just dirty needles :( Get out quick!", null);
+function item(id, name, desc) {
+		this.id = id;
+		this.name = name;
+		this.desc = desc;
+
+		function toString(){
+			return desc;
+		}
+}
+
+//items
+var sturgeon = new item(0, "Sweet Sturgeon", "Wow, what a fresh fish.");
+//locations
+var river = new location(0, "The River", "5. You're drowning in the river. But there is a sturgeon! Get out quick!","5. You're drowning in the river. No fish to be found just dirty needles :( Get out quick!", sturgeon);
+var hancock = new location(1, "Hancock", "u at hancock","5. You're drowning in the river. No fish to be found just dirty needles :( Get out quick!", null);
+var gym = new location(2, "Gym", "gym boizzut there is a sturgeon! Get out quick!","5. You're drowning in the river. No fish to be found just dirty needles :( Get out quick!", null);
+
+var inventory = [null,null,null]
 
 var map = [
 				     //n  s  e  w 
@@ -30,15 +45,19 @@ var currentLocation = 0;
 
 function north(){
 	currentLocation = map[currentLocation][0];
+	visitRoom();
 }
 function south(){
 	currentLocation = map[currentLocation][1];
+	visitRoom();
 }
 function east(){
 	currentLocation = map[currentLocation][2];
+	visitRoom();
 }
 function west(){
 	currentLocation = map[currentLocation][3];
+	visitRoom();
 }
 
 function visitRoom(){
@@ -46,8 +65,8 @@ function visitRoom(){
 	var message = site.desc;
 
 	if(site.visited == false){
-		score +=5
-		site.visited = true
+		score +=5;
+		site.visited = true;
 	}
 
 	//button disable code :)
@@ -70,6 +89,26 @@ function visitRoom(){
 	}
 
 	writeText(message);
+}
+
+function take(){
+	//get our current location
+	var site = locations[currentLocation];
+	//if theres no item, tell the player!
+	if(site.item == null){
+		writeText("There's no item to take Anthony!");
+	}
+	else{
+		var item = site.item;
+		//add item to inventory
+		inventory[item.id]=item;
+		//remove item from location
+		site.item=null;
+		//change site description to reflect the item is gone!
+		site.desc=site.descAfter;
+		writeText("You recieve: "+item.name);
+	}
+
 }
 
  // Writes to the text area
