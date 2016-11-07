258b06a8ee51fef8a9d135dda1bd433b6e70a43a
michelleydaley <mdaley.hhs@gmail.com>
2016-11-07 14:49:34 -0500

    figuring out map

--- utilities.js
+++ utilities.js
@@ -28,7 +28,10 @@ var sting sword = new item(2, "Sting Sword", "This glows when orcs are nearby.")
 var orcs finger = new item(3, "Orcs Finger", "A trophy for enduring the pain.");
 var lembas bread = new item(4, "Lembas Bread", "Even the smallest bite can fill your stomach for days.");
 var gollum = new item(5, "Gollum", "How can you be sure he'll be true to you?");
-var faramir = new item(6, "Faramir", "")
+var faramir = new item(6, )
+var giant spider shelob web = new itme(7, "Giant Spider Shelob Web", "Don't get caught in the web!");
+var small knife = new item(8, "Small Knife", "For anyone that gets in your way from now on...");
+var friends = new itme(9, "Friends", "Alas Reunited with your loved ones!");
 //locations
 var the shire = new location(0, "The Shire", "5. Frodo- you must leave the shire immediately... The Black Riders are following close behind!","5. You must not wear the ring or they will see you!", Ring);
 var weathertop = new location(1, "Weathertop", "A Black Rider has followed you! Don't put on the ring!","They sense that you're wearing the ring!", Mithril Armor);
@@ -37,18 +40,25 @@ var mines moria = new location(3, "Mines of Moria", "Doors of During: Speal frie
 var lothlorien = new location(4, "Elven Kingdom of Lothlorien","Ah! I think Lady Galadriel has a few gifts for you.","Can you speak Elvish? Are you sur eyo're worth of these gifts?",Lembas Bread);
 var amon hen = new location(5, "Amon Hen","The ring is yours Frodo... do not be fooled by Boromir.","Leave! The orcs are coming- travel to Mordor at last!",Small Ship);
 var emyn muil and dead marshes = new location(6, "Emyn Muil and Dead Marshes","Do you hear someone or someTHING behind you?","Sam, what's that?!", Gollum);
-var black gate = new location(7, "Black Gate","You cannot simply charge the gate. Head towards Ithilien.","You've been stopped in Gondor by Boromir's brother!", Faramir);
-var tower cirith ungol = new location(8, "Tower of Cirith Ungol","Follow Gollum up the steps of the tower...", "Watch out!", Giant Spider Shelob Web);
-var mordor = new location(9, "Mordor","You're so close to Mount Doom... If only there was some nourishment.","Quick! Gollum is trying to steal the ring from you!", Small Knife);
-var mount doom = new locatoin(10, "Mount Doom","The Eye of Sauron is watching you. Get Gollum out of the way!","You're finally free of the ring, Frodo!", Friends);
+var tower cirith ungol = new location(7, "Tower of Cirith Ungol","Follow Gollum up the steps of the tower...", "Watch out!", Giant Spider Shelob Web);
+var mordor = new location(8, "Mordor","You're so close to Mount Doom... If only there was some nourishment.","Quick! Gollum is trying to steal the ring from you!", Small Knife);
+var mount doom = new locatoin(9, "Mount Doom","The Eye of Sauron is watching you. Get Gollum out of the way!","You're finally free of the ring, Frodo!", Friends);
 
 var inventory = [null,null,null,null];
 
 var map = [
-				     //n  s  e  w 
-/*loc0 aka river*/    [1, 2, 0, 0],
-/*loc1 aka hancock*/  [1, 0, 1, 1],
-/*loc2 aka gym*/      [0, 1, 2, 2]
+				          //n  s  e  w 
+/*loc0 aka the shire*/    [0, 1, 0, 3],
+/*loc1 aka weathertop*/   [1, 2, 4, 0],
+/*loc2 aka rivendale*/    [3, 0, 2, 5],
+/*loc3 aka mines moria*/  [2, 5, 1, 6],
+/*loc4 aka lothlorien*/   [4, 2, 3, 7],
+/*loc5 aka amon hen*/     [5, 4, 6, 8],
+/*loc6 aka emyn muil*/    [7, 6, 5, 9],
+/*loc7 aka tower*/        [8, 5, 7, 6],
+/*loc8 aka mordor*/       [6, 7, 4, 9],
+/*loc9 aka mount doom*/   [9, 8, 7, 9]
+
 ];
 				// 0    1       2
 var locations = [river,hancock,gym];
